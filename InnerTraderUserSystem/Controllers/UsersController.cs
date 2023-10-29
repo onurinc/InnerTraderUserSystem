263974a1997ead197cc378f82e933b22e41168c5
onurinc <onur.i@hotmail.nl>
2023-10-30 00:39:53 +0100

    Add project files.

---
+++ InnerTraderUserSystem/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InnerTraderUserSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet(template:"GetAllUsers")]
+        public IActionResult Get()
+        {
+            return Ok("Test");
+
+        }
+
+    }
+}
---
+++ InnerTraderUserSystem/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace InnerTraderUserSystem.Models
+{
+    public class User
+    {
+
+        public Guid IdentityId { get; set; } = Guid.NewGuid();
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UserName { get; set; }
+
+    }
+}
